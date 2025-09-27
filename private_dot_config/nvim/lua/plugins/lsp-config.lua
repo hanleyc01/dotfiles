e6feb7a7fb2fa8ee844068b2376dc2fc88b3b640
Connor Hanley <connordhanley@gmail.com>
2025-09-27 13:10:10 -0400

    actually fixed lspconfig

---
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+    -- mason.nvim
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+    },
+
+    -- mason-lspconfig
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "ts_ls",
+                "rust_analyzer",
+                "clangd",
+            },
+        },
+    },
+
+    -- nvim-lspconfig
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("pyright")
+            vim.lsp.enable("ts_ls")
+            vim.lsp.enable("rust_analyzer")
+            vim.lsp.enable("clangd")
+        end
+    },
+}
