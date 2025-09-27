e75d661b3d320525c8f2e45edd7bc8ec4951d24f
Connor Hanley <connordhanley@gmail.com>
2025-09-27 12:56:47 -0400

    modular day-night colortheme

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 -- Set the colorscheme depending on the time of day.
-local function callback()
-    local current_hour = os.date("*t").hour
-    local is_nighttime = current_hour >= 15 or current_hour < 6
-    if is_nighttime then
-        vim.cmd([[colorscheme tokyonight-storm]])
-    else
-        vim.cmd([[colorscheme tokyonight-day]])
+local function callback(daytime_name, nighttime_name)
+    return function()
+        local current_hour = os.date("*t").hour
+        local is_nighttime = current_hour >= 15 or current_hour < 6
+        if is_nighttime then
+            vim.cmd("colorscheme " .. nighttime_name)
+        else
+            vim.cmd("colorscheme " .. daytime_name)
+        end
     end
 end
 
@@ -14,6 +16,6 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        init = callback,
+        init = callback("tokyonight-day", "tokyonight-storm"),
     }
 }
